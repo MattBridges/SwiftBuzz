0da9639698504c8a97254389c8300bc00262cb32
MattBridges <Matthew.Bridges1@gmail.com>
2023-01-18 11:10:06 -0500

    Added Input Verifications for each method

--- js/site.js
+++ js/site.js
@@ -5,21 +5,39 @@ function GetValues() {
   let buzzValue = parseInt(document.getElementById('buzzValue').value);
   let stopValue = parseInt(document.getElementById('stopValue').value);
 
-  //Check to make sure only numbers were entered
-  if (Number.isInteger(swiftValue) && Number.isInteger(buzzValue) && Number.isInteger(stopValue)) {
-    //If all are numbers Generate FizzBuzz then display it.
-    let possibleValues = GenerateFizzBuzz(swiftValue, buzzValue, stopValue);
-    DisplayFizzBuzz(possibleValues);
-
-  }
-  else {
-    //If they are not, tell the user
+//Check to make sure only numbers were entered
+if (Number.isInteger(swiftValue) && Number.isInteger(buzzValue) && Number.isInteger(stopValue)) {
+  if(stopValue>5000){
+    //If the stop value is larger than 5000 to protect the page from crashing 
     Swal.fire({
       icon: 'error',
       title: 'Oops!',
-      text: 'Only integers are allowed for SwiftBuzz'
+      text: 'Stop Value should be less than 5000'
     });
-  }
+    }
+    else if(swiftValue==0||buzzValue==0){
+    //Make sure the user didnt enter a 0 value for swift or buzz
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops!',
+      text: 'Swift and Buzz values cannot be 0'
+    });
+    }
+    else{
+       //If all are numbers Generate FizzBuzz then display it.
+    let possibleValues = GenerateFizzBuzzDry(swiftValue, buzzValue, stopValue);
+    DisplayFizzBuzz(possibleValues);
+    }
+}
+else 
+    {
+      //If they are not, tell the user
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops!',
+        text: 'Only integers are allowed for SwiftBuzz'
+      });
+    }
 
 
 }
--- js/ternary_site.js
+++ js/ternary_site.js
@@ -7,19 +7,37 @@ function GetValuesTurnary() {
 
   //Check to make sure only numbers were entered
   if (Number.isInteger(swiftValue) && Number.isInteger(buzzValue) && Number.isInteger(stopValue)) {
-    //If all are numbers Generate FizzBuzz then display it.
-    let possibleValues = GenerateFizzBuzzTernary(swiftValue, buzzValue, stopValue);
-    DisplayFizzBuzz(possibleValues);
-
+    if(stopValue>5000){
+      //If the stop value is larger than 5000 to protect the page from crashing 
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops!',
+        text: 'Stop Value should be less than 5000'
+      });
+      }
+      else if(swiftValue==0||buzzValue==0){
+      //Make sure the user didnt enter a 0 value for swift or buzz
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops!',
+        text: 'Swift and Buzz values cannot be 0'
+      });
+      }
+      else{
+         //If all are numbers Generate FizzBuzz then display it.
+      let possibleValues = GenerateFizzBuzzDry(swiftValue, buzzValue, stopValue);
+      DisplayFizzBuzz(possibleValues);
+      }
   }
-  else {
-    //If they are not, tell the user
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops!',
-      text: 'Only integers are allowed for SwiftBuzz'
-    });
-  }
+  else 
+      {
+        //If they are not, tell the user
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops!',
+          text: 'Only integers are allowed for SwiftBuzz'
+        });
+      }
 
 
 }
